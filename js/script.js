35f4cf6995ef0658577dbf35334f69415ec7830f
Layla <FalconsMathClub13@gmail.com>
2024-02-09 11:54:16 -0800

    developed public website

---
+++ js/script.js
@@ -0,0 +1,26 @@
+// var elements = document.querySelectorAll('.tabs');
+// var instance = M.Tabs.init(elements);
+
+// document.addEventListener('DOMContentLoaded', function() {
+//     var elems = document.querySelectorAll('.parallax');
+//     var instances = M.Parallax.init(elems);
+//   });
+// document.addEventListener('DOMContentLoaded', function() {
+//     var elems = document.querySelectorAll('.carousel');
+//    var options = {
+//      padding: 200,
+//      indicators: true
+//    }
+//     var instances = M.Carousel.init(elems, options);
+//   });
+
+// document.addEventListener('DOMContentLoaded', function() {
+//     var elems = document.querySelectorAll('.collapsible');
+//     var options = {
+//     inDuration: 500
+//     }
+//     var instances = M.Collapsible.init(elems, options);})
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.collapsible');
+    var instances = M.Collapsible.init(elems, options);
+  });
