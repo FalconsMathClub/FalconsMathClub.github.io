478c35a3564fb9579f350e74f7cf45e56b63aaca
Layla <FalconsMathClub13@gmail.com>
2024-04-05 11:55:26 -0700

    adding a password and fixing the background

--- js/script.js
+++ js/script.js
@@ -30,3 +30,12 @@ document.addEventListener('DOMContentLoaded', function () {
 })
 
 M.Sidenav.init(document.querySelectorAll('.sidenav'), {edge: 'left'});
+
+document.getElementById("submit").addEventListener("click", () => {
+  let pass = document.getElementById("password").value
+  let password = "FalconsMC"
+  if (pass == password){
+    document.getElementById("lesson-plans").classList.remove("hide")
+    document.getElementById("initial").classList.add("hide")
+  }
+})
