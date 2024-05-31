a55d2c62c91a94bf49612dc9f7cb6f6d78de7a85
Layla <FalconsMathClub13@gmail.com>
2024-05-31 14:01:21 -0700

    fixing spacing, trying to add a carousel

--- js/script.js
+++ js/script.js
@@ -39,3 +39,17 @@ document.getElementById("submit").addEventListener("click", () => {
     document.getElementById("initial").classList.add("hide")
   }
 })
+
+
+
+
+document.addEventListener('DOMContentLoaded', function() {
+  var elems = document.querySelectorAll('.carousel');
+  var instances = M.Carousel.init(elems, options);
+});
+
+// Or with jQuery
+
+$(document).ready(function(){
+  $('.carousel').carousel();
+});
