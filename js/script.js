a58e6b0698b5b094270699bec12ca3e696a9303b
Layla <FalconsMathClub13@gmail.com>
2024-05-31 14:10:20 -0700

    hopefully fixing the javascript issue

--- js/script.js
+++ js/script.js
@@ -39,17 +39,3 @@ document.getElementById("submit").addEventListener("click", () => {
     document.getElementById("initial").classList.add("hide")
   }
 })
-
-
-
-
-document.addEventListener('DOMContentLoaded', function() {
-  var elems = document.querySelectorAll('.carousel');
-  var instances = M.Carousel.init(elems, options);
-});
-
-// Or with jQuery
-
-$(document).ready(function(){
-  $('.carousel').carousel();
-});
